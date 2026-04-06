3792580d39ca1f9ef07a37ef25744ddb69df4db9
Kevin Chhan <kevinchhan1@gmail.com>
2026-04-06 11:23:41 -0700

    remove failing test

--- test.js
+++ test.js
@@ -3,7 +3,4 @@ const assert = require('assert');
 // A simple test to simulate a passing CI check
 assert.strictEqual(1 + 1, 2, 'Math is broken!');
 
-// A simple test to simulate a failing CI check
-assert.strictEqual(1 + 1, 3, 'Math is broken!');
-
 console.log('Tests passed. Ready for deployment.');
